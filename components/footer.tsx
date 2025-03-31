87b6bfc748a9c0dadfc3f9860eb683dab3c29f4f
TeamImpleMentors <155422421+TeamImpleMentors@users.noreply.github.com>
2025-04-01 00:27:37 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="bg-muted py-12">
       <div className="container">
         <div className="mt-8 pt-8 border-t border-muted-foreground/20 text-center text-sm text-muted-foreground">
-          &copy; {new Date().getFullYear()} ImpleMentors. All rights reserved.
+          &copy; {new Date().getFullYear()} ImpleMentors & Kriday Sharma. All rights reserved.
         </div>
       </div>
     </footer>
